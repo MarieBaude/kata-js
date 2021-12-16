12db708434a33928634c3124083b889a21fd645d
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2021-12-16 15:12:55 +0100

    delete comment, all test

--- 2_fizzbuzz/test/02-fizzbuzz.test.js
+++ 2_fizzbuzz/test/02-fizzbuzz.test.js
@@ -2,14 +2,30 @@ const chai = require('chai');
 const fizzbuzz = require('../02-fizzbuzz');
 const assert = chai.assert;
 
-// describe('array', () => {
-//     it('should return array', () => {
-//         assert.isArray(fizzbuzz());
-//     })
-// }) 
+describe('array', () => {
+    it('should return array', () => {
+        assert.isArray(fizzbuzz());
+    })
+}) 
 
-describe('index', () => {
-    it('should ...', () => {
-        assert.lengthOf(fizzbuzz()[0], 1);
+describe('length', () => {
+    it('should return length of array', () => {
+        assert.lengthOf(fizzbuzz(), 1);
     })
 })
+
+describe('index 0', () => {
+    it('should return index 0 is a number', () => {
+        assert.isNumber(fizzbuzz()[0]);
+    })
+})
+
+describe('index 2', () => {
+    it('should return index 2 is a not number', () => {
+        assert.isNotNumber(fizzbuzz()[2]);
+    })
+})
+
+
+
+

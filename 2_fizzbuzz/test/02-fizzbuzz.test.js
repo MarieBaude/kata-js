dc600a734ca51d59c330525d59b1680a3a0c8181
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2021-12-16 14:33:33 +0100

    adding index 0 test

--- 2_fizzbuzz/test/02-fizzbuzz.test.js
+++ 2_fizzbuzz/test/02-fizzbuzz.test.js
@@ -2,8 +2,14 @@ const chai = require('chai');
 const fizzbuzz = require('../02-fizzbuzz');
 const assert = chai.assert;
 
-describe('array', () => {
-    it('should return array', () => {
-        assert.isArray(looping());
+// describe('array', () => {
+//     it('should return array', () => {
+//         assert.isArray(fizzbuzz());
+//     })
+// }) 
+
+describe('index', () => {
+    it('should ...', () => {
+        assert.lengthOf(fizzbuzz()[0], 1);
     })
-}) 
+})

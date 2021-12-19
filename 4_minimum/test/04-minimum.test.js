e3684cd2a7ec9236894b44ff53d949e618770107
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2021-12-19 17:21:32 +0100

    adding code

---
+++ 4_minimum/test/04-minimum.test.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const min = require('../04-minimum');
+const assert = chai.assert;
+
+describe('', () => {
+    it('should return ', () => {
+        assert.isNotString(min());
+    })
+}) 

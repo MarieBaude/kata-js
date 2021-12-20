910e75922f8a321e33805224de4b7c3e098bef42
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2021-12-20 14:34:19 +0100

    initial projet

---
+++ 6_counting/test/06-counting.test.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+const assert = chai.assert;
+import {counting} from '../06-counting.js';
+
+describe('isEven', () => {
+    it('should return ....', () => {
+        
+    })
+}) 

6a24f1412e5bb3af7666132d25eaf20e9501ebbb
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2021-12-20 13:40:05 +0100

    adding code kata

--- 5_recursion/05-recursion.js
+++ 5_recursion/05-recursion.js
@@ -1,3 +1,11 @@
-export function isEven () {
- 
-}
+export function isEven(num) {
+    if (num === 0) {
+      return true;
+    } else if (num === 1) {
+      return false;
+    } else {
+      return isEven(num - 2);
+    }
+  }
+  
+  isEven(50);

25d1b0100514d71e5be42309d1878e07916d0631
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2021-12-20 16:08:43 +0100

    adding test

--- 6_counting/06-counting.js
+++ 6_counting/06-counting.js
@@ -1,8 +1,7 @@
 export function countBs (majB) {
     return (majB.split("B").length - 1)
 }
-  
+
 export function countChar (word, letter) {
     return (word.split(letter).length - 1)
 }
-  
--- 6_counting/test/06-counting.test.js
+++ 6_counting/test/06-counting.test.js
@@ -3,8 +3,26 @@ const assert = chai.assert;
 import {countBs} from '../06-counting.js';
 import {countChar} from '../06-counting.js';
 
-describe('isEven', () => {
-    it('should return ....', () => {
-        
+describe('countBs', () => {
+    it('should return is ok', () => {
+        assert.isOk(countBs('BBC'))
+    }),
+    it('should return is a string', () => {
+        assert.isNumber(countBs('BBC'))
+    }),
+    it('should return BBC has 2 B', () => {
+        assert.equal(countBs('BBC'), 2)
+    })
+}) 
+
+describe('countChar', () => {
+    it('should return is ok', () => {
+        assert.isOk(countChar('fufufu', 'f'))
+    }),
+    it('should return is a string', () => {
+        assert.isNumber(countChar('fufufu', 'f'))
+    }),
+    it('should return fufufu has 3 f', () => {
+        assert.equal(countChar('fufufu', 'f'), 3)
     })
 }) 
